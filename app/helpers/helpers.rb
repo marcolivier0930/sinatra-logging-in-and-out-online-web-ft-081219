ec9673872e90aa910dad707d9ea5dc80a0625cf6
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-09 03:42:38 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-  def current_user(session)
-    
+  def self.current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
   end
   
-  def is_logged_in?
+  def self.is_logged_in?
     
   end
 end
